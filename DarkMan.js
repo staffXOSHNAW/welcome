50db6d6ab02d0e155685e36f5424b30e0a00d08f
YaSHEX11 <69230403+YaSHEX11@users.noreply.github.com>
2021-07-02 02:39:19 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -25,7 +25,7 @@ const DarkMan = new Discord.Client();
 const prefix = "y-";
 
 
-DarkMan.login("ODUxMzA3MTU2NjkwNjk4Mjgw.YL2Xkg.s-sNAyYQBEScTYcguoECOgtCTiA");
+DarkMan.login("ODUxMzA3MTU2NjkwNjk4Mjgw.YL2Xkg.Koyd1oBug50PuoFVhB_hRUStxS4");
 DarkMan.on("ready", async () => {
   console.log(`Logged in as ${DarkMan.user.username}!`);
   DarkMan.user.setStatus("ONLINE");
